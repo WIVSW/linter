bd0d0e2acc33c9dd6284b241bcc32fce65468c6b
WIVSW <olegspostnikov@gmail.com>
2019-06-19 08:57:38 +0500

    Add content vertical space test

---
+++ src/tests/form/content-vertical-space.js
@@ -0,0 +1,49 @@
+const Test = require('../test.js');
+const ContentVerticalSpaceError =
+	require('../../models/errors/form/content-vertical-space.js');
+/* eslint-disable no-unused-vars */
+/* import types for GCC */
+const Block = require('../../models/block.js');
+const BlockCollection = require('../../collections/block.js');
+/* eslint-enable no-unused-vars */
+
+/**
+ */
+class ContentVerticalSpace extends Test {
+	/**
+	 * @param {BlockCollection} collection
+	 */
+	constructor(collection) {
+		super({
+			Model: ContentVerticalSpaceError,
+			collection,
+		});
+	}
+
+	/**
+	 * @param {BlockCollection} collection
+	 * @return {Array<Block>}
+	 * @protected
+	 */
+	_selectBlocks(collection) {
+		return collection
+			.getBlocksByName('form')
+			.filter((block) => block.children.length);
+	}
+
+	/**
+	 * @param {Block} block
+	 * @return {boolean}
+	 * @protected
+	 */
+	_isValidBlock(block) {
+		return this._collection
+			.getAllBlockChidren(block)
+			.filter((child) => child.isFormTextElement() && child.mods['size'])
+			.map((child) => child.mods['size'])
+			.filter((val, i, self) => self.indexOf(val) === i)
+			.length <= 1;
+	}
+}
+
+module.exports = ContentVerticalSpace;
